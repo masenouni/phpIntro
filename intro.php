ae07da4c1d9f324d4dd901d48e8d7bc410928555
winstone <winstoneothis@gmail.com>
2022-01-27 11:19:34 +0300

    initial commit

--- intro.php
+++ intro.php
@@ -1,1 +1,26 @@
 <?php
+echo "Welcome to my first explorer class training";
+echo "<br>";
+echo "This is my first class in php";
+echo "<br>";
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>php</title>
+</head>
+<body>
+<?php
+
+echo "how is php taking you so far";
+echo "<br>";
+echo 95 + 80;
+?>
+
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,30 @@
+<?php
+
+$x="12300";
+$y="650";
+//The sum of two variables.
+echo "The sum is:";
+
+echo  $x + $y;
+
+echo "<br>";
+echo "<br>";
+//The difference of the two variables.
+echo "The difference is:";
+
+echo  $x - $y;
+
+echo "<br>";
+echo "<br>";
+//The product of two numbers.
+echo "The product is:";
+
+echo  $x * $y;
+
+echo "<br>";
+echo "<br>";
+
+//The division.
+echo "The Division is:";
+
+echo  $x / $y;
