58f932602d33a593a9510d7ab114c2e32732479c
winstone <winstoneothis@gmail.com>
2022-01-31 10:20:31 +0300

    update

---
+++ operators.php
@@ -0,0 +1,57 @@
+<?php
+//Arithmetic operators
+$num1 = 106;
+$num2 = 217;
+
+echo $num1+$num2;//addition
+
+echo "<br>";
+
+echo $num1-$num2;//subtraction
+
+echo "<br>";
+
+echo $num1*$num2;//Multiplication
+
+echo "<br>";
+
+echo $num1/$num2;//Division
+
+echo "<br>";
+
+echo $num1%$num2;//modulus
+
+echo "<br>";
+
+//Assignment Operators
+
+$num3=876;
+$num3 +=761;
+
+echo $num3;
+echo "<br>";
+
+//Comparison Operator.
+$num4 = 640;
+$num5 = "640";
+
+var_dump($num4==$num5);//equals operator
+
+echo "<br>";
+
+var_dump($num4===$num5);//identical operator
+
+echo "<br>";
+
+var_dump($num4!=$num5);//Not equals operator
+
+echo "<br>";
+
+$num6=400;
+$num7=400;
+
+var_dump($num6>$num7);//greater
+
+
+
+
