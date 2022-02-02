3463d8303e7b4e6adff8dd457aef2b71caaa922c
winstone <winstoneothis@gmail.com>
2022-02-02 10:13:46 +0300

    loops

---
+++ loop.php
@@ -0,0 +1,19 @@
+<?php
+//the while loop
+
+$Marks=50;//initializing my loop
+
+while($Marks<=150){
+    echo "The Marks is: $Marks <br>";
+    $Marks+=5;
+}
+echo "<br>";
+
+//The do...while loop
+
+$age=50;
+do{
+    echo "Age is: $age <br>";
+    $age+=5;
+
+}while($age<=100);
